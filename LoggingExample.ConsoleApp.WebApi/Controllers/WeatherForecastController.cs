151a84bc8511d25243b563ebaa784b8328947c1e
sanneiphyo <sanneiphyo@gmail.com>
2025-01-30 17:10:05 +0630

    Feat : Logger webapi

---
+++ LoggingExample.ConsoleApp.WebApi/Controllers/WeatherForecastController.cs
@@ -0,0 +1,39 @@
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace LoggingExample.ConsoleApp.WebApi.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet(Name = "GetWeatherForecast")]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Getting weather forecast -- Start");
+            var lst = Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+                TemperatureC = Random.Shared.Next(-20, 55),
+                Summary = Summaries[Random.Shared.Next(Summaries.Length)]
+            })
+            .ToArray();
+
+            _logger.LogInformation($"List => {JsonConvert.SerializeObject(lst ,Formatting.Indented)}");
+            _logger.LogInformation("Getting weather forecast -- End");
+            return lst;
+        }
+    }
+}
