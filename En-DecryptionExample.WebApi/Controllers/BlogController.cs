f25c2ece53f039377e8614d69baa26cd7686b68a
sanneiphyo <sanneiphyo@gmail.com>
2025-01-31 16:36:22 +0630

    token

---
+++ En-DecryptionExample.WebApi/Controllers/BlogController.cs
@@ -0,0 +1,95 @@
+﻿using System.Text.Json.Serialization;
+using En_DecryptionExample.WebApi.Service;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+
+namespace En_DecryptionExample.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BlogController : ControllerBase
+    {
+        private readonly EncDecService _encDecService;
+
+        public BlogController(EncDecService encDecService)
+        {
+            _encDecService = encDecService;
+        }
+
+        [HttpPost]
+        public IActionResult Login(BlogLoginRequestModel requestModel)
+        {
+            try
+            {
+                var result = UserData.Users.FirstOrDefault(x =>
+                  x.UserName == requestModel.UserName &&
+                  x.Password == requestModel.Password);
+
+                if (result is null) 
+                {
+                    return Unauthorized();
+                }
+
+                var user = new BlogLoginModel
+                {
+                    SessionExpired = DateTime.Now.AddMinutes(1),
+                    SessionId = Guid.NewGuid().ToString(),
+                    UserName = result.UserName
+                };
+
+                var json = JsonConvert.SerializeObject(user);
+
+                var token = _encDecService.Encrypt(json);
+
+                _encDecService.Encrypt(result.UserName);
+                return Ok();
+            }
+            catch(Exception ex) 
+            {
+               return StatusCode(500,ex.ToString());
+            }
+        }
+    }
+
+}
+
+public static class UserData
+{
+    public static List<UserDto> Users = new List<UserDto>
+    {
+        new UserDto
+        {
+            UserName = "admin",
+            Password = "admin"
+        },
+        new UserDto
+        {
+            UserName = "user",
+            Password = "user"
+        }
+    };
+}
+
+    public class BlogLoginRequestModel //controller => RequestModel
+{
+    public string UserName { get; set; }
+    public string Password { get; set; }
+}
+
+public class BlogLoginModel
+{
+    public string UserName { get; set; }
+    public string SessionId { get; set; }
+
+    public DateTime SessionExpired { get; set; }
+}
+public class UserDto //Database Mapping
+{
+    public string UserName { get; set; }
+
+    public string Password { get; set; }
+
+}
+
+//15;10
---
+++ En-DecryptionExample.WebApi/Service/EncDecService.cs
@@ -0,0 +1,30 @@
+﻿using System.Text;
+using Effortless.Net.Encryption;
+
+namespace En_DecryptionExample.WebApi.Service
+{
+    public class EncDecService
+    {
+
+       private readonly byte[] key = Encoding.ASCII.GetBytes("1234567890123456");//string to bytes
+       private readonly byte[] iv = Encoding.ASCII.GetBytes("1234567890123456");
+
+        public EncDecService(IConfiguration configuration)
+        {
+            key = Encoding.ASCII.GetBytes(configuration["Security : Key"]!);
+            iv = Encoding.ASCII.GetBytes(configuration["Security : IV"]!);
+        }
+
+        public string Encrypt(string plainText)
+        {
+            return Strings.Encrypt(plainText, key, iv);
+        }
+
+        public string Decrypt(string plainText)
+        {
+            return Strings.Decrypt(plainText, key, iv);
+        }
+
+       
+    }
+}
